9f3626c0c637b691c83c380e2eae7a6a954c3c49
Tom-Erik Valsø <tomerikv@gmail.com>
2026-07-13 16:46:04 +0200

    alt2

---
+++ src/main/java/com/extra/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.extra.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
